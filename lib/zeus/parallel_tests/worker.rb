9327cfa3a2d863a94c02338e24c352b190a7dc0d
Johannes Barre <igel@igels.net>
2016-11-11 14:49:59 +0100

    asht

--- lib/zeus/parallel_tests/worker.rb
+++ lib/zeus/parallel_tests/worker.rb
@@ -21,7 +21,6 @@ module Zeus
       end
 
       def spawn
-        pp caller
         puts "spawn: #{%(zeus parallel_#{@suite}_worker #{parallel_tests_attributes}).inspect}"
         system %(zeus parallel_#{@suite}_worker #{parallel_tests_attributes})
         args_file.unlink
